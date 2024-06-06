88543b893e763f9afb9597de858d95fc4934ae7f
Naïm Abdelkerim <naimdevelopper@gmail.com>
2024-06-06 20:38:17 +0100

    fix: change app port

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 9090,
+  },
 
   resolve: {
     alias: {
